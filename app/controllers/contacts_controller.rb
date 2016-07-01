36712e98cad551306cd47ac2e4563a09e9e8e65a
cloverharvest <anna@schwabdev.com>
2016-07-01 00:46:13 -0700

    Adds new contacts form, index, new and create actions, saves to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,25 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
   def new
-    @contact = Contact.new
+    @contact = Contact.new(id: params[:id])
   end
 
   def create
+    @contact = Contact.new(category_params)
+    if @contact.save
+      flash[:success] = "New contact added."
+      redirect_to contacts_path
+    else
+      flash[:error] = @category.errors.full_messages.to_sentence
+      render :new
+    end
   end
+
+  private
+    def category_params
+      params.require(:contact).permit(:name, :email, :comments, :id)
+    end
 end
