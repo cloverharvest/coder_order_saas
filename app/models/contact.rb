2d1f6365773d49b9248cb00126958990159c0018
cloverharvest <anna@schwabdev.com>
2016-07-01 14:29:36 -0700

    Form validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  validates :name, :email, :comments, presence: true
+  validate :comments, length: { in: 5..1000, too_long: "%#{count} characters is the maximum allowed"}
 end
