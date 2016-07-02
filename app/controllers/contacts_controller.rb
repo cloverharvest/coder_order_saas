6bb57d76232347c368dd6deda8f4368bcf8edb27
cloverharvest <anna@schwabdev.com>
2016-07-02 00:56:27 -0700

    Body to comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,8 +12,8 @@ class ContactsController < ApplicationController
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
-      body = params[:contact][:body]
-      ContactMailer.contact_email(name, email, body).deliver
+      comments = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, comments).deliver
 
       flash[:success] = "Message sent successfully."
       redirect_to new_contact_path
