c846f90bd035fcb8238c8c078912544704be461d
cloverharvest <anna@schwabdev.com>
2016-07-02 00:06:09 -0700

    Make mailer work

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'anna@schwabdev.com'
+  default to: 'anna.pesigan@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
