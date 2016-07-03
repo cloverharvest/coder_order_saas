a6aa488368255d0e31f5d135fc68fff0bad73717
cloverharvest <anna@schwabdev.com>
2016-07-03 00:08:17 -0700

    Adds other files generated by user model and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,16 @@ Rails.application.routes.draw do
 
 
 
-#         root GET    /                            pages#home
-#        about GET    /about(.:format)             pages#about
+# Prefix Verb   URI Pattern                  Controller#Action
+#    users_new GET    /users/new(.:format)         users#new
+#        users GET    /users(.:format)             users#index
+#              POST   /users(.:format)             users#create
+#     new_user GET    /users/new(.:format)         users#new
+#    edit_user GET    /users/:id/edit(.:format)    users#edit
+#         user GET    /users/:id(.:format)         users#show
+#              PATCH  /users/:id(.:format)         users#update
+#              PUT    /users/:id(.:format)         users#update
+#              DELETE /users/:id(.:format)         users#destroy
 #     contacts GET    /contacts(.:format)          contacts#index
 #              POST   /contacts(.:format)          contacts#create
 #  new_contact GET    /contacts/new(.:format)      contacts#new
@@ -23,5 +31,7 @@ Rails.application.routes.draw do
 #              PATCH  /contacts/:id(.:format)      contacts#update
 #              PUT    /contacts/:id(.:format)      contacts#update
 #              DELETE /contacts/:id(.:format)      contacts#destroy
+#        about GET    /about(.:format)             pages#about
+#         root GET    /                            pages#home
 
 end
