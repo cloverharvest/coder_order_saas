e6ddd94fd9d55379c2b29ee0a6bef37f3176d654
cloverharvest <anna@schwabdev.com>
2016-07-05 14:24:14 -0700

    Writes test for valid/invalid name and email addresses

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,12 +21,28 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should not be too long" do
-    @user.name = "a" * 50
-    assert @user.valid?
+    @user.name = "a" * 51
+    assert_not @user.valid?
   end
 
   test "email should not be too long" do
     @user.email = "a" * 50 + "@example.com"
     assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@fizz.com abc123@def.org first.last@buzz.jp anna+joe@baz.co]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should not accept invalid addresses" do
+    invalid_addresses = %w[user@example,com USER_at_foo.org abc123@def anna+joe@baz_bar.co joe@baz+bar.co]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
