a1454525851c7babfe9e17dc3a1dce804a833f21
cloverharvest <anna@schwabdev.com>
2016-07-09 00:55:25 -0700

    Cleans up flow for sign up errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => "pages#home"
   get "/about", to: "pages#about", as: "about"
 
-  get "/users", to: "users#index", as: "users"
+  # get "/users", to: "users#index", as: "users"
   get "/sign_up", to: "users#new", as: "sign_up"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
