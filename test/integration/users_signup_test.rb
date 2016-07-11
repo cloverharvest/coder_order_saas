7c5c0799f86844f42bbb459f0bd8896bbf1c8f61
cloverharvest <anna@schwabdev.com>
2016-07-10 18:47:38 -0700

    Adds test for successful sign-up and flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get sign_up_path
     assert_no_difference 'User.count' do
-      post users_path, params: {user: { name: "",
+      post users_path, params: {user: { name:  "",
                                         email: "user@invalid",
                                         password:              "foo",
                                         password_confirmation: "bar" } }
@@ -13,5 +13,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     # assert_select 'div#<CSS id for error explanation>'
     # assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "valid signup information" do
+    get sign_up_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name:  "Example User",
+                                        email: "user@example.com",
+                                        password: "password",
+                                        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
+
   end
 end
