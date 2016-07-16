8fed6291076d289330c80e8efcecd93abcf2e8c8
cloverharvest <anna@schwabdev.com>
2016-07-15 18:48:07 -0700

    Adds authenticated? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,17 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
-  
+
   # remembers a user in the database for use in persistent sessions (cookies)
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true of the given token matches the digest
+  # the ff. remember_token is a variable local to the authenticated? method and
+  # NOT connected in any way to the remember_token on line 2
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
