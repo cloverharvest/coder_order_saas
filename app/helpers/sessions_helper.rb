ef375c496617821d4024fbe4358f2d6c56094169
cloverharvest <anna@schwabdev.com>
2016-07-15 18:59:59 -0700

    Adds remember user in controller, remember user as a helper, and comments

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,14 +5,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # returns the current logged in user, if any
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # returns true, if the user is logged in, returns false otherwise
   def logged_in?
     !current_user.nil?
   end
 
+  # logs out the user
   def log_out
     session.delete(:user_id)
     @current_user = nil
