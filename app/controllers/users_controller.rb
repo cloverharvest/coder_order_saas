bee1a5260a39b393243e005076fbecb5c938b81e
cloverharvest <anna@schwabdev.com>
2016-08-04 00:36:35 -0700

    Makes test for successful edit, adds specs for update action and allow_nil:true for pw edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,12 +30,13 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Code here
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render :edit
     end
   end
-    
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
