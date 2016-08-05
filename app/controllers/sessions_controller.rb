0dbee6a5de62ec2e969d25d877c0c381d4b6069d
cloverharvest <anna@schwabdev.com>
2016-08-05 01:27:47 -0700

    Advanced sign-in, authorization pending

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = "Invalid email or password"
+      flash.now[:danger] = "The email or password somehow don't match up."
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def index
   end
@@ -40,5 +41,15 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    #Before filters
+
+    #Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Log in please."
+        redirect_to login_url
+      end
     end
 end
