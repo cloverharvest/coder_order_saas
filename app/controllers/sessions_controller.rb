ee9904cddf43df6a605e04303bbb3ec4c169fd7c
cloverharvest <anna@schwabdev.com>
2016-08-07 00:24:56 -0700

    More friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
-class SessionsController < ApplicationController
+  class SessionsController < ApplicationController
   def new
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = "The email or password somehow don't match up."
       render :new
